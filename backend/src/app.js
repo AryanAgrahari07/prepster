53fc92cfb19ead011061db092b0499dad7da8a80
AryanAgrahari07 <aryanagrahari52666@gmail.com>
2026-06-08 23:46:46 +0530

    fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,8 @@ const allowedOrigins = [
   process.env.FRONTEND_URL || 'http://localhost:5173',
   'http://localhost:5173',
   'http://localhost:3000',
+  'https://prepster.online',
+  'https://www.prepster.online',
 ];
 
 app.use(cors({
